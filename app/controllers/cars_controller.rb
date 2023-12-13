b0578b0d1fa9e7a2e8161c983d39bf115050bfea
Yeiner CQ <ycarvajalq@gmail.com>
2023-12-13 15:38:40 -0500

    add import logic for csv file

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,5 @@
 class CarsController < ApplicationController
+  require "csv"
   before_action :set_file, only: [:import]
   def index
     @cars = Car.all
@@ -6,6 +7,14 @@ class CarsController < ApplicationController
 
   def import
     return redirect_to cars_path, notice: "csv file only, please!" unless @file.content_type == "text/csv"
+    @file = File.open(@file)
+    csv = CSV.parse(@file, headers: true)
+    cars = []
+    csv.each do |row|
+      cars << Car.new(row.to_h)
+    end
+    # debugger
+    Car.import(cars)
     redirect_to cars_path, notice: "Data has been imported successfully."
   end
 
